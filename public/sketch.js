fe6519e2fccd5e94f92f8a200c54f2c8c21fc7dd
Saurabh Datta <saurabh.datta@frogdesign.com>
2017-04-03 20:35:17 +0800

    first commit

---
+++ public/sketch.js
@@ -0,0 +1,71 @@
+var capture;
+
+var cp;
+var gui;
+
+
+function setup() {
+    createCanvas(640, 480);
+
+    capture = createCapture(VIDEO);
+    capture.size(640, 480);
+    capture.hide();
+
+    cp = new Controls();
+    gui = new dat.GUI();
+
+    initGUI();
+}
+
+function draw() {
+    background(0);
+    image(capture, 0, 0);
+}
+
+var initGUI = function() {
+    gui.add(cp, 'Image_count', 1, 10).step(1);
+    gui.add(cp, 'Save_images');
+};
+
+function b64_creation(img_data){
+    // print("-----------------");
+    // print(img_data.filename);
+    // print(img_data.ext);
+    
+    // extract raw bs64 data
+    // var raw_bs64_data = String(img_data.imageData);
+    // var extracted_bs64_data = raw_bs64_data.replace(/^data\:image\/\w+\;base64\,/, '');
+    // print(extracted_bs64_data);
+    // print("-----------------");
+    // print(" ");
+    // 
+    
+              
+    $.ajax({
+        type: "POST",
+        url: "http://10.202.217.19:3000/img_sent/",
+        data: "Hello World post",
+        success: function(msg){
+             alert('wow '+ msg);
+           },
+        dataType: String
+    });
+}
+
+function save_frames_server(data){
+    print(data);
+    for (var i = 0; i < data.length; i ++){
+        b64_creation(data[i]);
+    }
+}
+
+
+var Controls = function() {
+    this.Image_count = 4;
+
+    this.Save_images = function() {
+        saveFrames("frames", "png", this.Image_count, 1, function(data){
+            save_frames_server(data);
+        });
+    };
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+var port = 3000;
+
+var server = app.listen(port);
+
+app.use(express.static('public'));
+
+app.get('/img_sent/', function(req, res) {
+  console.log(req.body);
+  console.log("received something");
+  res.send('ok saved');
+});
+
+if (server) {
+    console.log("server running at port: " + port);
+}
+
