2a21407b67888f1addae8c2467e480fa315214f7
Saurabh Datta <saurabh.datta@frogdesign.com>
2017-04-04 17:00:14 +0800

    bs64 to file trial One

--- public/sketch.js
+++ public/sketch.js
@@ -2,7 +2,9 @@ var capture;
 
 var cp;
 var gui;
-
+var counter = 0;
+var saved = false;
+var number_of_images = 0;
 
 function setup() {
     createCanvas(640, 480);
@@ -21,14 +23,53 @@ function draw() {
     background(0);
     image(capture, 0, 0);
 }
-
+var speeder ;
 var initGUI = function() {
     gui.add(cp, 'Image_count', 1, 10).step(1);
     gui.add(cp, 'Save_images');
+    gui.add(cp, 'Train_images');
+    gui.add(cp, 'Show_future');
 };
 
+var Controls = function() {
+    this.Image_count = 4;
+
+    this.Save_images = function() {
+        saveFrames("frames", "jpg", this.Image_count, 1, function(data){
+            save_frames_server(data);
+        });
+    };
+
+    this.Train_images = function(){
+        // ask server to train data
+    };
+
+    this.Show_future = function(){
+        // ask server to train data
+    };
+
+};
+
+function save_frames_server(data){
+    // print(data);
+    for (var i = 0; i < data.length; i ++){
+        b64_creation(data[i]);
+    }
+}
+
 function b64_creation(img_data){
-    print(img_data);
+    // print(img_data);
+
+    var raw_bs6_data = img_data.imageData;
+    // print(raw_bs6_data);
+    // var sliced_bs6_data = raw_bs6_data.slice(24, raw_bs6_data.length);
+    var sliced_bs6_data = raw_bs6_data.replace(/^data:image\/octet-stream;base64,/,'');
+    // print(sliced_bs6_data);
+
+    img_data.imageData = sliced_bs6_data;
+
+    // var raw_image_data = img_data.imageData.splice(24, img_data.imageData.length);
+    // img_data.imageData = raw_image_data;
 
     $.ajax({
         type: "POST",
@@ -39,26 +80,8 @@ function b64_creation(img_data){
                 print(msg);
             }else{
                 print("didn't get the msg");
+                window.alert("didn't get the msg");
             }
         }
     });
 }
-
-function save_frames_server(data){
-    print(data);
-    for (var i = 0; i < data.length; i ++){
-        b64_creation(data[i]);
-    }
-}
-
-
-var Controls = function() {
-    this.Image_count = 4;
-
-    this.Save_images = function() {
-        saveFrames("frames", "jpg", this.Image_count, 1, function(data){
-            save_frames_server(data);
-        });
-
-    };
-};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var app = express();
 var port = 3000;
@@ -11,9 +12,17 @@ app.use(bodyParser.json({type: 'application/*+json', limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.post('/img_sent/', function(req, res) {
-  // it is supposed to read "Hello World post" here
-  console.log("received something");
-  console.log(req.body);
+  // console.log("received something");
+  // console.log(req.body);
+  // 
+  var img_object = req.body;
+  console.log(img_object.filename);
+  console.log(img_object.ext);
+  console.log(img_object.imageData);
+
+  var buff = new Buffer(img_object.imageData, 'base64');
+  fs.writeFileSync('/data/' + img_object.filename + img_object.ext, buff);
+  
   res.send('ok saved');
 });
 
