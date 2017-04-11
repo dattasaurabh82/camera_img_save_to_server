38635752dd37c1296061df698145c361ef2a28cc
Saurabh Datta <saurabh.datta@frogdesign.com>
2017-04-11 20:47:06 +0800

    updated the easter egg

--- public/easterEgg.js
+++ public/easterEgg.js
@@ -1,4 +1,112 @@
 var domAnimator = new DomAnimator();
-domAnimator.addFrame('o_o');
-domAnimator.addFrame('-_o');
-domAnimator.animate();
+// domAnimator.addFrame('a');
+// domAnimator.addFrame('au');
+// domAnimator.addFrame('auto');
+// domAnimator.addFrame('autom');
+// domAnimator.addFrame('automa');
+// domAnimator.addFrame('automat');
+// domAnimator.addFrame('automato');
+// domAnimator.addFrame('a tomato');
+// domAnimator.addFrame('auto ato');
+// domAnimator.addFrame('au omato');
+// domAnimator.addFrame('aut mato');
+// domAnimator.addFrame('a tom to');
+// domAnimator.addFrame('aut mato');
+// domAnimator.addFrame('automato.farm');
+var frame1 = ['     _I_      ',
+              '    /o o\\    ',
+              '    | - |     ',
+              '   -------    ',
+              '  /|     |\\  ',
+              ' / |_____| \\ ',
+              '      |       ',
+              '     {0}      '
+              ];
+
+var frame2 = ['     _I_      ',
+              '    /  o\\    ',
+              '    |  .|     ',
+              '   -------    ',
+              '   |    /|    ',
+              '   |___/_|    ',
+              '      /       ',
+              '    {@}       '
+              ];
+
+var frame3 = ['       _I_      ',
+              '      /  o\\    ',
+              '      |  .|     ',
+              '     -------    ',
+              '     |    /|    ',
+              '     |___/_|    ',
+              '        /       ',
+              '      {@}       '
+              ];
+var frame3 = ['         _I_      ',
+              '        /  o\\    ',
+              '        |  .|     ',
+              '       -------    ',
+              '       |    /|    ',
+              '       |___/_|    ',
+              '          /       ',
+              '        {@}       '
+              ];
+var frame3 = ['              _I_      ',
+              '             /  o\\    ',
+              '             |  .|     ',
+              '            -------    ',
+              '            |    /|    ',
+              '            |___/_|    ',
+              '               /       ',
+              '             {@}       '
+              ];
+var frame4 = ['              _I_      ',
+              '             /o o\\    ',
+              '             | - |     ',
+              '            -------    ',
+              '           /|     |\\  ',
+              '          / |_____| \\ ',
+              '               |       ',
+              '              {0}      '
+              ];
+
+var frame5 = ['              _I_      ',
+              '             /o  \\    ',
+              '             |.  |     ',
+              '            -------    ',
+              '            |\\     |  ',
+              '            |_\\ ___|  ',
+              '               \\      ',
+              '               {@}     '
+              ];
+
+var frame6 = ['           _I_      ',
+              '          /o  \\    ',
+              '          |.  |     ',
+              '         -------    ',
+              '         |\\     |  ',
+              '         |_\\ ___|  ',
+              '            \\      ',
+              '            {@}     '
+              ];
+
+
+var frame7 = ['       _I_      ',
+              '      /o  \\    ',
+              '      |.  |     ',
+              '     -------    ',
+              '     |\\     |  ',
+              '     |_\\ ___|  ',
+              '        \\      ',
+              '        {@}     '
+              ];
+
+
+domAnimator.addFrame(frame1);
+domAnimator.addFrame(frame2);
+domAnimator.addFrame(frame3);
+domAnimator.addFrame(frame4);
+domAnimator.addFrame(frame5);
+domAnimator.addFrame(frame6);
+domAnimator.addFrame(frame7);
+domAnimator.animate(150);
