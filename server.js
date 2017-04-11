4b739c6925bc3b2bc91d5d5feb1e9963ff5b8513
Saurabh Datta <saurabh.datta@frogdesign.com>
2017-04-11 16:15:37 +0800

    https enabled for remote connection from phones

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+var https = require('https');
 var fs = require('fs');
+var bodyParser = require('body-parser');
 var shell = require('shelljs');
 
-var app = express();
-var port = 3000;
 
+var port = 3000;
+var PORT = process.env.PORT || 5000;
+var HOST = process.env.HOST || '';
+
+var app = express();
 var server = app.listen(port);
 
 
@@ -28,9 +32,9 @@ app.post('/clean_data/', function(req, res){
 
 app.post('/img_sent/', function(req, res) {
   var img_object = req.body;
-  // // console.log(img_object.filename);
-  // // console.log(img_object.ext);
-  // // console.log(img_object.imageData);
+  // console.log(img_object.filename);
+  // console.log(img_object.ext);
+  // console.log(img_object.imageData);
 
   var buff = new Buffer(img_object.imageData, 'base64');
   fs.writeFileSync('data/' + img_object.filename + '.' +img_object.ext, buff);
@@ -38,9 +42,18 @@ app.post('/img_sent/', function(req, res) {
   res.send('ok saved');
 });
 
+var secure_options = {
+    key  : fs.readFileSync('ssl/key.pem'),
+    ca   : fs.readFileSync('ssl/csr.pem'),
+    cert : fs.readFileSync('ssl/cert.pem')
+};
 
 
 if (server) {
     console.log("server running at port: " + port);
 }
+
+https.createServer(secure_options, app).listen(PORT, HOST, null, function() {
+    console.log('secure Server listening on port %d in %s mode', this.address().port, app.settings.env);
+});
 
